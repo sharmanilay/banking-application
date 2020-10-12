b9936a4a7a88940029342ad575bf1518203fded9
Nilay Sharma <nilaysharma642@gmail.com>
2020-10-12 20:50:34 +0530

    setting up backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,7 +29,76 @@ Router.post('/signup', async (req, res) => {
     );
 
     if (isInvalidFieldProvided) {
-      return res.status(400).send
+      return res.status(400).send({
+        signup_error: 'Invalid field.'
+      });
     }
+
+    const result = await pool.query(
+      'select count(*) as count from bank_user where email=$1',
+      [email]
+    );
+    const count = result.rows[0].count;
+    if (count > 0) {
+      return res.status(400).send({
+        signup_error: 'User with this email address already exists'
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+    await pool.query(
+      'insert into bank_user(first_name, last_name, email, password) values($1, $2, $3, $4)',
+      [first_name, last_name, email, hashedPassword]
+    )
+    res.status.(400).send();
+  } catch (error) {
+    res.status(400).send({
+      signup_error: 'Error signing up...Try again later.'
+    })
   }
-})
+});
+
+Router.post('/signin', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await validateUser(email, password);
+    if (!user) {
+      res.status(400).send({
+        signin_error: 'Email/Password does not match.'
+      });
+    }
+
+    const token = await generateAuthToken(user);
+    const result = await pool.query(
+      'insert into tokens(access_token, userid) values($1, $2) returning *',
+      [token, user.userid]
+    );
+    if (!result.rows[0]) {
+      return res.status(400).send({
+        signin_error: 'Error signing in...Try again later.'
+      });
+    }
+    user.token = result.rows[0].access_token;
+  } catch (error) {
+    res.status(400).send({
+      signin_error: 'Email/password does not match.'
+    });
+  }
+});
+
+Router.post('/logout', authMiddleware, async (req, res) => {
+  try {
+    const { userid, access_token } = req.user;
+    await pool.query(
+      'delete from tokens where userid=$1 and access_token=$2',
+      [userid, access_token]
+    );
+    res.send(400);
+  } catch (error) {
+    res.status(400).send({
+      logout_error: 'Error while logging out..Try again later.'
+    });
+  }
+});
+
+module.exports = Router;
